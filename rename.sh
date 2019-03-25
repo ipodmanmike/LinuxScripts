6882575215721dbeab62b2b96db0c8448f0daf7e
ipodmanmike <mike.fcb10@gmail.com>
2019-03-25 02:10:23 +0000

    add file rename script

---
+++ rename.sh
@@ -0,0 +1,8 @@
+
+Based on the @Prince John Wesley answer, here is a simple bash script for changing all extensions of files in the current directory from ext1 to ext2. Also outputs names of the files being renamed.
+
+#!/bin/bash
+for f in *.$1
+do
+    [ -f "$f" ] && mv -v "$f" "${f%$1}$2"
+done
